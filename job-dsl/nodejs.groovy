805fcd943daaba660ccc4f4679574e4f9d6a1938
Mauro Andocilla <m.andocilla@me.com>
2020-01-17 14:23:15 -0500

    change dockerhub & github repositories in job-dsl/nodejsdocker.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,6 +1,6 @@
-job('NodeJS example') {
+job('NodeJS example DSL') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('git://github.com/mauroandocilla/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,8 +1,8 @@
-job('NodeJS Docker example') {
+job('NodeJS Docker example DSL') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('git://github.com/mauroandocilla/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('Mauro DSL User')
+            node / gitConfigEmail('m.andocilla@me.com')
         }
     }
     triggers {
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('cargo77/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
